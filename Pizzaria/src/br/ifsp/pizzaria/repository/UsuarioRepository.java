87f735b2748cc888e5db9b722015735ce3218bc2
DiRock <araujo.die93@gmail.com>
2016-09-05 11:16:33 -0300

    Classe repository de usuário adicionada

---
+++ Pizzaria/src/br/ifsp/pizzaria/repository/UsuarioRepository.java
@@ -0,0 +1,39 @@
+package br.ifsp.pizzaria.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import br.ifsp.pizzaria.entities.Usuario;
+
+public class UsuarioRepository {
+	private EntityManager manager;
+	
+	public UsuarioRepository(EntityManager manager){
+		this.manager = manager;
+	}
+	
+	public void adiciona(Usuario usuario){
+		this.manager.persist(usuario);
+	}
+	
+	public Usuario busca(int id){
+		return this.manager.find(Usuario.class, id);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Usuario verificarLogin(Usuario usuario){
+		try{
+			Query query = this.manager.createQuery("FROM Usuario  WHERE login = :login AND senha = :senha");
+			query.setParameter("login", usuario.getLogin());
+			query.setParameter("senha", usuario.getSenha());
+			return (Usuario) query.getSingleResult();
+		}
+		catch(NoResultException e){
+			return null;
+		}
+		
+		
+	}
+	
+}
